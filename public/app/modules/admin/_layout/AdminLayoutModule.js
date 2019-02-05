f48f1be4cac296333d8525a8f8f83cede1a592c7
grish97 <44112653+grish97@users.noreply.github.com>
2019-02-05 13:55:38 +0400

    simple Change log out

--- public/app/modules/admin/_layout/AdminLayoutModule.js
+++ public/app/modules/admin/_layout/AdminLayoutModule.js
@@ -1,8 +1,9 @@
-APP.controller('AdminHeaderController', function ($scope, authManager) {
+APP.controller('AdminHeaderController', function ($scope, authManager,$state) {
     $scope.admin_logout = function ($event) {
         $event.preventDefault();
         localStorage.removeItem('api_admin_token');
         authManager.unauthenticate();
+        $state.go('login');
     }
 });
 
